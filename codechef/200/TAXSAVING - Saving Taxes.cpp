3679bfe9bd2dc56ec8cbcd948e634ea2116ce874
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-02 00:01:06 +0530

    Add Saving Taxes (200)

---
+++ codechef/200/TAXSAVING - Saving Taxes.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << X - Y << endl;
+    }
+
+    return 0;
+}
