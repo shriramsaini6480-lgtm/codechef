0119f330b55ffd9a099e0a04af4d0130dd627142
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-02 00:03:35 +0530

    Add Donation Drive (200)

---
+++ codechef/200/DONDRIVE - Donation Drive.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+      int n,x;
+      cin>>n>>x;
+     if(n-x==0){
+        cout<<"0"<<endl; 
+     }
+     else{
+         cout<<n-x<<endl;
+     }
+  }
+   
+}
