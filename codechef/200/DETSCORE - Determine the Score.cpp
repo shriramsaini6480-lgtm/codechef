24abaaf0bd3fa10311bd076266d025183ac91af8
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-02 00:02:23 +0530

    Add Determine the Score (200)

---
+++ codechef/200/DETSCORE - Determine the Score.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,n;
+        cin>>x>>n;
+        cout<<(x*n)/10<<endl;
+    }
+  
+   
+}
