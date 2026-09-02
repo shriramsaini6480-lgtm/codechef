0b024f25dc9597fe88fd17cdcbf6d83b3121a93e
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-02 10:23:54 +0530

    Add Who is taller! (200)

---
+++ codechef/200/TALLER - Who is taller!.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+ int t;
+ cin>>t;
+ for(int i=0;i<t;i++){
+     int x,y;
+     cin>>x>>y;
+     if(x>y){
+         cout<<"a"<<endl;
+     }
+     else{
+         cout<<"b"<<endl;
+     }
+ }
+   
+}
