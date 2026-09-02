3f777d60449810c1190038e1e6526379e096596c
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-02 21:00:44 +0530

    Add Reach Weight (Unrated)

---
+++ codechef/Unrated/REACHWT - Reach Weight.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (n % 2 == 0)
+            cout << (n / 2) * 30 << endl;
+        else
+            cout << (n / 2) * 30 + 20 << endl;
+    }
+
+    return 0;
+}
