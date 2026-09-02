8872fe1828405842bd3ab44183eba0b9f4665256
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-02 10:18:31 +0530

    Add Bone Appetit (200)

---
+++ codechef/200/BNE_APT - Bone Appetit.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n,m,x,y;
+  cin>>n>>m;
+  cin>>x>>y;
+  int bones,blood;
+  bones=n*x;
+  blood=m*y;
+  cout<<bones+blood<<endl;
+  
+   
+}
