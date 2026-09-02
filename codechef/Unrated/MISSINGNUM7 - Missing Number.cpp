de93f077818d74ea3bd558c35dea87929478b10b
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-02 21:43:43 +0530

    Add Missing Number (Unrated)

--- codechef/Unrated/MISSINGNUM7 - Missing Number.cpp
+++ codechef/Unrated/MISSINGNUM7 - Missing Number.cpp
@@ -4,17 +4,18 @@ using namespace std;
 int main(){
   int s;
   cin>>s;
-   if(s<10&&s>8){
-       cout<<"1"<<endl;
-   }
-   else if (s>7&&s<9){
-       cout<<"2"<<endl;
+   //if(s<10&&s>8){
+    //   cout<<"1"<<endl;
+   //}
+   //else if (s>7&&s<9){
+      // cout<<"2"<<endl;
        
-   }
-   else if(s>6&&s<8){
-       cout<<"3"<<endl;
-   }
-   else{
-       cout<<"4"<<endl;
-   }
+   //}
+   //else if(s>6&&s<8){
+      // cout<<"3"<<endl;
+   //}
+   //else{
+    //   cout<<"4"<<endl;
+  // }*/
+  cout<<(10-s)<<endl;
 }
