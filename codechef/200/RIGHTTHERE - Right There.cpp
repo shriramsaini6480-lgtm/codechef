7a6fd5f8468862da4cb2aa020f703468a967bfc4
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-03 22:03:33 +0530

    Add Right There (200)

---
+++ codechef/200/RIGHTTHERE - Right There.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+ int t;
+ cin>>t;
+ while(t--){
+     int n,x;
+     cin>>n>>x;
+     if(x>=n){
+         cout<<"yes"<<endl;
+     }
+     else{
+         cout<<"no"<<endl;
+     }
+ }
+   
+}
