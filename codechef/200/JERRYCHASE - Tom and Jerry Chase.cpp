3dda895720053e78fbe0fa92e8b9aad5bc0904e4
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-03 21:49:43 +0530

    Add Tom and Jerry Chase (200)

---
+++ codechef/200/JERRYCHASE - Tom and Jerry Chase.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+      int x,y;
+      cin>>x>>y;
+      if(y>x){
+          cout<<"yes"<<endl;
+      }
+      else{
+          cout<<"no"<<endl;
+      }
+  }
+   
+}
