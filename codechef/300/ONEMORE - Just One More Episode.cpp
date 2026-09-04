1c509cda6e3e10bc27559f8816dcdf6972e866f6
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-04 10:56:59 +0530

    Add Just One More Episode (300)

---
+++ codechef/300/ONEMORE - Just One More Episode.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+ int t;
+ cin>>t;
+ while(t--){
+     int x;
+     cin>>x;
+     if(24<x){
+         cout<<"yes"<<endl;
+     }
+     else{
+         cout<<"no"<<endl;
+     }
+ }
+   
+}
