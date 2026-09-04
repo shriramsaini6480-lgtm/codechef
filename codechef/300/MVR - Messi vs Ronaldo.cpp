924e3ec3f6404a099148dbfeff2c3427e5ac2ac5
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-04 10:37:06 +0530

    Add Messi vs Ronaldo (300)

---
+++ codechef/300/MVR - Messi vs Ronaldo.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a,b,x,y;
+  cin>>a>>b>>x>>y;
+  if((a*2+b*1)>(x*2+y*1)){
+      cout<<"messi"<<endl;
+  }
+  else if((a*2+b*1)<(x*2+y*1)){
+      cout<<"ronaldo"<<endl;
+  }
+  else{
+      cout<<"equal"<<endl;
+  }
+   
+}
