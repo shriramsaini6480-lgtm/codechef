45f05b0d1590361543c5aabda7cde47658f0b34a
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-04 10:49:37 +0530

    Add October Marathon (300)

---
+++ codechef/300/OCTATHON - October Marathon.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int x;
+  cin>>x;
+  if(x<3){
+      cout<<"gold"<<endl;
+  }
+  else if (x>=3&&x<6){
+      cout<<"silver"<<endl;
+  }
+  else{
+      cout<<"bronze"<<endl;
+  }
+   
+}
