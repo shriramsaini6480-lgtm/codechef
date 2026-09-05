7c6ee6f9534b44cecbb04412553d72e391b83875
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-05 22:12:20 +0530

    Add Rain in Chefland (300)

---
+++ codechef/300/RAINFALL1 - Rain in Chefland.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+      int x;
+      cin>>x;
+      if(x<3){
+          cout<<"light"<<endl;
+      }
+      else if(x>=3&&x<7){
+          cout<<"moderate"<<endl;
+      }
+      else{
+          cout<<"heavy"<<endl;
+          
+      }
+  }
+   
+}
