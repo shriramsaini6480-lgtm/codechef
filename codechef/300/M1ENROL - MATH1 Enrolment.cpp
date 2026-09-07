a983e57148704a92936047ba77604cda1a31d369
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-08 00:36:57 +0530

    Add MATH1 Enrolment (300)

---
+++ codechef/300/M1ENROL - MATH1 Enrolment.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+ int t;
+ cin>>t;
+ while(t--){
+     int x,y;
+     cin>>x>>y;
+     if(y>=x)
+{
+  cout<<(y-x)<<endl; 
+} 
+else{
+    cout<<"0"<<endl;
+}
+   
+}
+}
