a6252e74f3feffc4d1037d3330fd14edb28b594b
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-08 00:11:20 +0530

    Add Passes for Fair (300)

---
+++ codechef/300/FAIRPASS - Passes for Fair.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+      int n,k;
+      cin>>n>>k;
+      if(k>n){
+          cout<<"yes"<<endl;
+      }
+      else if(k==n){
+          cout<<"no"<<endl;
+      }
+      else{
+          cout<<"no"<<endl;
+      }
+      
+  }
+   
+}
