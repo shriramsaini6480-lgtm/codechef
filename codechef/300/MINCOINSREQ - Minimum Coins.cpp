8ca027169a5d5359920d1b53b19bb77daddbdd58
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-12 16:38:16 +0530

    Add Minimum Coins (300)

---
+++ codechef/300/MINCOINSREQ - Minimum Coins.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        cout << X % 10 << '\n';
+    }
+
+    return 0;
+}
