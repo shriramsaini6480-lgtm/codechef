10bd3c9cb959f26a71c4307316c969e8d5b9b90b
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-12 16:33:24 +0530

    Add Chef and Wire Frames (300)

---
+++ codechef/300/CWIREFRAME - Chef and Wire Frames.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+      int n,m,x;
+      cin>>n>>m>>x;
+      cout<<(x*2*(n+m))<<endl;
+  }
+   
+}
