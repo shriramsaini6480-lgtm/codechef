907112f0703f9262e17644ecc90e793ea7d89f3e
shriramsaini009 <shriramsaini6480@gmail.com>
2026-09-12 20:24:54 +0530

    Add ATM (400)

---
+++ codechef/400/HS08TEST - ATM.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int x;
+    double y;
+    if (cin >> x >> y) {
+        if (x % 5 == 0 && y >= x + 0.50) {
+            y -= (x + 0.50);
+        }
+        cout << fixed << setprecision(2) << y << endl;
+    }
+    return 0;
+}
